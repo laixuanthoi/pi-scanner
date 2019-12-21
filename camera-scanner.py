d9fcf9407e3a71b41b0f20eacb65e8d148397b99
laixuanthoi <laixuanthoi@gmail.com>
2019-12-21 09:25:21 +0700

    okk

--- camera-scanner.py
+++ camera-scanner.py
@@ -1,11 +1,24 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
-
-capture = cv2.VideoCapture(0)
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+# image = rawCapture.array
+ 
+# # display the image on screen and wait for a keypress
+# # cv2.imshow("Image", image)
+# cv2.waitKey(0)
 
 while True:
-    ret, frame = capture.read()
-    if not ret:
-        break
-    # cv2.imshow("frame", frame)
-    print("okkkk")
+    image = rawCapture.array
+    print(image)
     cv2.waitKey(10)
